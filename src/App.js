2c610d99ee7741449bdbe9b07d1ae596fa7fa805
dldnjswns31 <dldnjswns32@gmail.com>
2022-10-30 02:56:11 +0900

    feature(IssueContext): Context API 파일 생성 및 List 페이지에 적용

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import GlobalStyle from './shared/GlobalStyle';
 import Routers from './Router';
 import Header from './components/shared/Header';
+import IssueContext from './context/IssueContext';
+import { useEffect, useState } from 'react';
+import { apis } from './shared/axios';
 
 const App = () => {
+  const [issues, setIssues] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
+  useEffect(() => {
+    apis.getIssues(1).then((res) => setIssues([...res.data]));
+  }, []);
   return (
     <>
       <GlobalStyle />
       <Header />
-      <Routers />
+      <IssueContext.Provider value={issues}>
+        <Routers />
+      </IssueContext.Provider>
     </>
   );
 };
--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,7 +1,14 @@
 import ListFrame from '../components/List/ListFrame';
+import IssueContext from '../context/IssueContext';
 
 const List = () => {
-  return <ListFrame />;
+  return (
+    <IssueContext.Consumer>
+      {(issues) => {
+        return <ListFrame issues={issues} />;
+      }}
+    </IssueContext.Consumer>
+  );
 };
 
 export default List;
