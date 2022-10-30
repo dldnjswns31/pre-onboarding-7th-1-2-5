e2513833f17731a43f0ac030fca6848d70895da3
dldnjswns31 <dldnjswns32@gmail.com>
2022-10-31 06:08:59 +0900

    feature(List): Context API를 통해 상태 관리하도록 로직 수정
    - ListFrame 컴포넌트의 Context Provider 삭제

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,14 +1,7 @@
 import ListFrame from '../components/List/ListFrame';
-import IssueContext from '../context/IssueContext';
 
 const List = () => {
-  return (
-    <IssueContext.Consumer>
-      {(obj) => {
-        return <ListFrame obj={obj} />;
-      }}
-    </IssueContext.Consumer>
-  );
+  return <ListFrame />;
 };
 
 export default List;
