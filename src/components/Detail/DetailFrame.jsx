c3a1c91f267840a5d5db01178c1cd38d8a8788e4
dldnjswns31 <dldnjswns32@gmail.com>
2022-10-30 13:00:31 +0900

    design(DetailFrame): DetailFrame 컴포넌트 프레임 완성

---
+++ src/components/Detail/DetailFrame.jsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+import IssueInfo from './IssueInfo';
+
+const DetailFrame = ({ issue }) => {
+  return (
+    <StMain>
+      <StContainer>
+        <IssueInfo issue={issue} />
+      </StContainer>
+    </StMain>
+  );
+};
+
+export default DetailFrame;
+
+const StMain = styled.main`
+  display: flex;
+  width: 100vw;
+  height: calc(100vh - 8rem);
+  margin-top: 8rem;
+  justify-content: center;
+`;
+
+const StContainer = styled.div`
+  width: 100%;
+  max-width: calc(1200px + 4rem);
+  margin: 0 2rem;
+`;
