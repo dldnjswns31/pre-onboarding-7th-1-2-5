2c418d1f8566d5854e3bf8b62b7418b3c2cceffc
WonJun <71388830+dldnjswns31@users.noreply.github.com>
2022-12-21 20:46:08 +0900

    feat: markup editor color mode를 light mode로 고정

--- src/components/Detail/IssueContent.jsx
+++ src/components/Detail/IssueContent.jsx
@@ -1,7 +1,11 @@
 import MDEditor from '@uiw/react-md-editor';
 
 const IssueContent = ({ issue }) => {
-  return <MDEditor.Markdown source={issue.body} style={{ padding: 32, backgroundColor: '#FAFAFA' }} />;
+  return (
+    <div data-color-mode="light">
+      <MDEditor.Markdown source={issue.body} style={{ padding: 32, backgroundColor: '#FAFAFA' }} />;
+    </div>
+  )
 };
 
 export default IssueContent;
